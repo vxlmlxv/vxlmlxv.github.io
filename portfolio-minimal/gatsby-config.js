4fe5a6a7b5d4fafee587fc07c1066f34877c83de
Lee Ye Seul <vxlmlxv@gmail.com>
2025-07-18 15:47:31 +0900

    edit favicon svg

--- portfolio-minimal/gatsby-config.js
+++ portfolio-minimal/gatsby-config.js
@@ -5,14 +5,13 @@ module.exports = {
       options: {
         siteUrl: "https://gatsby-starter-portfolio-minimal-theme.netlify.app/", // Used for sitemap generation
         manifestSettings: {
+          favicon: "./content/images/emoji-favicon.svg", // Emoji favicon
           siteName: "Yeseul's blog", // Used in manifest.json
           shortName: "Yeseul", // Used in manifest.json
           startUrl: "/", // Used in manifest.json
           backgroundColor: "#FFFFFF", // Used in manifest.json
           themeColor: "#000000", // Used in manifest.json
           display: "minimal-ui", // Used in manifest.json
-          icon: `data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">❤️‍🔥</text></svg>`, // Replace 🌟 with your preferred emoji
-
         },
         contentDirectory: "./content",
         blogSettings: {
